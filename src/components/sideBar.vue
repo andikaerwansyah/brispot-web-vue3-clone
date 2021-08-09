4f49686cdec4011ce6bc2e5f50b8a945a945337e
andika erwansyah <andika.erwansyah@gmail.com>
2021-08-09 09:21:40 +0700

    Initial commit
    phase one  brispot clone with vue.js v3 , tailwindcss and heroicons

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,3 +1,134 @@
 <template>
-  <div class="bg-grey h-full"></div>
+  <div class="sticky top-0 w-64 bg-brispot">
+    <div class="flex-1 fixed mt-5">
+      <div class="flex-shrink-0 ml-3">
+        <router-link to="/">
+          <img
+            class="h-8 w-33"
+            src="../assets/brispotweb_logos.png"
+            alt="BRISpot Web Logo"
+          />
+        </router-link>
+      </div>
+      <!-- Scrollable Container -->
+      <div
+        class="overflow-y-scroll top-0 h-screen w-64 brispot-scroll-bar mt-3"
+      >
+        <template v-for="item in navigation" :key="item">
+          <nav
+            class="
+              text-sm text-white
+              py-2
+              px-4
+              hover:bg-gray-200
+              hover:text-yellow-600
+              focus:border-l-2
+              rounded
+              align-middle
+            "
+          >
+            <router-link
+              :to="item.url"
+              class="
+                whitespace-nowrap
+                overflow-hidden overflow-ellipsis
+                w-60
+                align-middle
+              "
+            >
+              <div class="inline-block overflow-hidden align-middle">
+                <component :is="item.icon" class="h-5 w-5 text-white-500" />
+              </div>
+              <span class="ml-2">
+                {{ item.text }}
+              </span>
+            </router-link>
+          </nav>
+        </template>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import {
+  BeakerIcon,
+  HomeIcon,
+  UserAddIcon,
+  CheckCircleIcon,
+  DocumentReportIcon,
+  PencilAltIcon,
+  CollectionIcon,
+  ShieldCheckIcon,
+  ArchiveIcon,
+  ClipboardListIcon,
+  CogIcon,
+  IdentificationIcon,
+  LinkIcon,
+  OfficeBuildingIcon,
+  TemplateIcon,
+} from "@heroicons/vue/solid";
+
+const navigation = [
+  { text: "Home", url: "/", icon: "HomeIcon" },
+  { text: "Calon Penyimpan Potensial (CPP)", url: "/cpp", icon: "UserAddIcon" },
+  { text: "Prakarsa & Putusan Kredit", url: "/cpp", icon: "CheckCircleIcon" },
+  { text: "Pencairan Pinjaman", url: "/cpp", icon: "PencilAltIcon" },
+  { text: "Register SKPP", url: "/cpp", icon: "CollectionIcon" },
+  { text: "Asuransi", url: "/cpp", icon: "ShieldCheckIcon" },
+  { text: "Pengelolaan Agunan Lain", url: "/cpp", icon: "ArchiveIcon" },
+  { text: "Pembinaan & Penagihan", url: "/cpp", icon: "ClipboardListIcon" },
+  { text: "Reporting", url: "/cpp", icon: "DocumentReportIcon" },
+  {
+    text: "Laporan Ligas BRIvolution",
+    url: "/cpp",
+    icon: "DocumentReportIcon",
+  },
+  { text: "Pengaturan", url: "/cpp", icon: "CogIcon" },
+  {
+    text: "Pin Cash Pickup",
+    url: "/pinCashPickup",
+    icon: "IdentificationIcon",
+  },
+  { text: "Valuechain", url: "/pinCashPickup", icon: "LinkIcon" },
+  { text: "Survey", url: "/pinCashPickup", icon: "ClipboardListIcon" },
+  { text: "Administrator", url: "/pinCashPickup", icon: "OfficeBuildingIcon" },
+  { text: "Lain Lain", url: "/pinCashPickup", icon: "TemplateIcon" },
+  { text: "About", url: "/about" },
+  { text: "About", url: "/about" },
+  { text: "About", url: "/about" },
+  { text: "About", url: "/about" },
+];
+
+export default {
+  components: {
+    BeakerIcon,
+    HomeIcon,
+    UserAddIcon,
+    CheckCircleIcon,
+    DocumentReportIcon,
+    PencilAltIcon,
+    CollectionIcon,
+    ShieldCheckIcon,
+    ArchiveIcon,
+    ClipboardListIcon,
+    CogIcon,
+    IdentificationIcon,
+    LinkIcon,
+    OfficeBuildingIcon,
+    TemplateIcon,
+  },
+  setup() {
+    return {
+      navigation,
+    };
+  },
+};
+</script>
+
+<style>
+.brispot-scroll-bar {
+  scrollbar-color: #ffd580 grey;
+  scrollbar-width: thin;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,67 @@
 <template>
-  <h1>User Profile</h1>
+  <div class="flex justify-between">
+    <template v-for="(item, index) in feStacks" :key="index">
+      <div
+        class="max-w-sm py-4 px-8 bg-white shadow-lg rounded-lg my-20 order-{{index == 0 ? `first` : {{index}}}}"
+      >
+        <div class="flex justify-center md:justify-end -mt-16">
+          <img
+            class="
+              w-20
+              h-20
+              object-scale-down
+              rounded-full
+              border-2 border-indigo-500
+            "
+            :src="item.img"
+          />
+        </div>
+        <h2 class="text-gray-800 text-3xl font-semibold">{{ item.title }}</h2>
+        <div class="h-40 overflow-auto">
+          <p class="mt-2 text-gray-600">
+            {{ item.text }}
+          </p>
+        </div>
+        <div class="flex justify-end mt-4">
+          <a
+            :href="item.url"
+            target="_blank"
+            class="text-xl font-medium text-indigo-500"
+            >Dokumentasi</a
+          >
+        </div>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
+let feStacks = [
+  {
+    title: "Vue.js V3",
+    text: "Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects.",
+    img: "https://raw.githubusercontent.com/weizhanzhan/vue3-ts-template-h5/master/src/assets/images/vue3.png",
+    url: "https://v3.vuejs.org/",
+  },
+  {
+    title: "Tailwind CSS",
+    text: "A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.",
+    img: "https://www.markusantonwolf.com/media/pages/blog/tailwind-css/265298487-1623549769/tailwind-css-logo.svg",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    title: "Heroicons",
+    text: "Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.",
+    img: "https://bestofjs.org/logos/heroicons.svg",
+    url: "https://vue-hero-icons.netlify.app/",
+  },
+];
+
+export default {
+  setup() {
+    return {
+      feStacks,
+    };
+  },
+};
 </script>
